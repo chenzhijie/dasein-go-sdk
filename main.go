a77650e12aa6c0685ddfe493901ecaa468f98d22
Guo XiaoJie <guoxiaojie@onchain.com>
2018-08-25 19:09:02 +0800

    Get child block Cid from dagNode.
    Get child block Cid from dagNode.
    
    Signed-off-by: Guo XiaoJie <guoxiaojie@onchain.com>

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"context"
 
-	core "github.com/daseinio/dasein-go-sdk/core"
-	fsrepo "github.com/ipfs/go-ipfs/repo/fsrepo"
-	cid "gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
+	ml "github.com/daseinio/dasein-go-sdk/merkledag"
+	"github.com/daseinio/dasein-go-sdk/core"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+
+	"gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
 )
 
 func main (){
@@ -28,7 +30,7 @@ func main (){
 		fmt.Printf(err.Error())
 	}
 
-	//c, err := cid.Decode("QmXoKv5VauAAFDmNzYNSGerHaFpAXXQTfz3nqBTzfYnYrD")
+	//c, err := cid.Decode("QmaUKtR4kfexCktV5MhwQas9voxSYJCTb4AccGUSeFKHiQ")
 	c, err := cid.Decode("QmbqsMj8Sv9NGhdhYrz8XDbkJbkpoL6fx4ePyLF7qRJuhn")
 	if err != nil {
 		fmt.Println(err.Error())
@@ -42,6 +44,20 @@ func main (){
 		fmt.Println(block.Cid().String())
 
 		fmt.Println("RawData: ")
-		fmt.Println(string(block.RawData()))
+		fmt.Println(len(block.RawData()))
+		fmt.Println(block.RawData())
+
+		dagNode, err := ml.DecodeProtobufBlock(block)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("Decode success")
+		}
+		lks := dagNode.Links()
+		for i := 0; i < len(lks); i++ {
+			fmt.Println(lks[i].Cid.String())
+		}
+
 	}
 }
+
